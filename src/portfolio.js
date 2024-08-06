de638b71420b5a942dc8698329d5b45373c171a0
Shreyas Kothari <spkothari0@gmail.com>
2024-08-06 15:56:20 -0400

    updated Resume Link

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1YPD4wugu7bSMyvMK-F4Q59w11C-07_-U/view?usp=sharing",
+    "https://drive.google.com/file/d/1YddjDYOxVshmv18IJ2PEJmsqVuxRPMft/view?usp=sharing",
   portfolio_repository: "https://github.com/spkothari0/Shreyas-Portfolio",
   githubProfile: "https://github.com/spkothari0",
 };
